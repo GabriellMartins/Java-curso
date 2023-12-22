f4007750da09ecdd309d5700fabc0178382f42eb
GabriellMartins <gabrielnpmartins@hotmail.com>
2023-12-22 09:49:20 -0300

    First commit

---
+++ src/main/java/com/gabrielmartins/Main.java
@@ -0,0 +1,27 @@
+package com.gabrielmartins;
+
+
+public class Main {
+
+    public static void main(String[] args ) {
+
+        int idade;
+        double salario, altura;
+        char genero;
+        String nome;
+
+        idade = 30;
+        salario = 300;
+        altura = 1.90;
+        genero = 'F';
+        nome = "Gabriel Martins Da Silva";
+
+        System.out.println("IDADE =  " + idade);
+        System.out.println("SALARIO =  " + salario);
+        System.out.println("GENERO =  " + genero);
+        System.out.println("NOME =  " + nome);
+        System.out.println("ALTURA =  " + altura);
+
+
+       }
+    }
